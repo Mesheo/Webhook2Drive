9c45c5abb7413e749af1b5ef77135aedc0b8f919
Michel Ledig <m.ledig.mic@gmail.com>
2024-01-24 04:13:50 -0300

    all working

--- google_api/google_drive.py
+++ google_api/google_drive.py
@@ -3,11 +3,11 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-from googleapiclient.http import MediaFileUpload
+from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
 import os
+import pandas as pd
 
-def drive_uploader(form_name):
-    print("[drive_uploder] - Iniciando comunicação com drive")
+def authentication_process():
     SCOPES = ["https://www.googleapis.com/auth/drive"]
 
     creds = None
@@ -26,20 +26,53 @@ def drive_uploader(form_name):
 
         with open('/tmp/token.json', 'w') as token:
             token.write(creds.to_json())
+    return creds
 
+def download_file(service, file_id, file_name, destination_path):
+    
+    request = service.files().get_media(fileId=file_id)
+
+    with open(destination_path, "wb") as file:
+        downloader = MediaIoBaseDownload(file, request)
+        done = False
+        while not done:
+            status, done = downloader.next_chunk()
+            print("Download %d%%." % int(status.progress() * 100))
+    print(f"Arquivo '{file_name}' baixado com sucesso como '{destination_path}'.")
+  
+
+def merge_csv_files(original_file, new_file, merged_file):
+    df_original = pd.read_csv(original_file)
+    df_new = pd.read_csv(new_file)
+
+    # Concatenar os DataFrames
+    df_merged = pd.concat([df_original, df_new], ignore_index=True)
+
+    # Salvar o DataFrame mesclado em um novo arquivo CSV
+    df_merged.to_csv(merged_file, index=False, encoding='utf-8')
+
+    print(f"Arquivos CSV mesclados com sucesso. Arquivo salvo como '{merged_file}'.")
+
+
+def drive_uploader(form_name):
+    print("[drive_uploader] - Iniciando comunicação com drive")
+    creds = authentication_process()
+
+    parent_folder_id = ''
     try:
         service = build("drive", "v3", credentials=creds)
-
         #Listando as pastas
         response = service.files().list(
-            q="name='Forms' and mimeType='application/vnd.google-apps.folder'",
+            q="name='VELVET_FORMS' and mimeType='application/vnd.google-apps.folder'",
             spaces='drive'
         ).execute()
+        print("AQUI as pastas: ", response)
 
         #Criando a pasta se não existe
         if not response['files']:
+            print("[drive_uploader] - Criando pasta VELVET_FORMS...")
             file_metadata = {
-                "name": "Forms",
+                "name": "VELVET_FORMS",
                 "mimeType": "application/vnd/google-apps.folder"
             }
 
@@ -48,20 +81,52 @@ def drive_uploader(form_name):
                 fields="id"
             ).execute()
 
-            folder_id = file.get('id')
+            parent_folder_id = file.get('id')
         else:
-            folder_id = response['files'][0]['id']
+            print("[drive_uploader] - Pasta VELVET_FORMS já existe!")
 
-        #populando a pasta
-        file_metadata = {
-            "name": form_name,
-            "parents": [folder_id] #adikcionando o file dentro da pasta
-        }
+            parent_folder_id = response['files'][0]['id']
+        print("Folder id: ", parent_folder_id)
 
-        media = MediaFileUpload(f'/tmp/output.csv')
-        upload_file = service.files().create(body=file_metadata,
-                                            media_body=media,
-                                                fields="id").execute()
+        #Verificando se ja tem um csv do formulario na pasta
+        result = service.files().list(
+                q=f"name='{form_name}' and '{parent_folder_id}' in parents",
+                spaces="drive",
+            ).execute()
+
+        form_file = result.get("files", [])
+        print("Arquivo encontrado: ", form_file)
+
+        if form_file:
+            form_file_id = form_file[0]["id"]
+            download_path = "/tmp/baixado.csv"
+
+            download_file(service, form_file_id, f'{form_name}.csv', download_path)
+            merge_csv_files("/tmp/output.csv", download_path, "/tmp/merged_output.csv")
+
+             # Upload do novo arquivo mesclado
+            media = MediaFileUpload("/tmp/merged_output.csv")
+            request = service.files().update(
+                fileId=form_file_id,
+                media_body=media
+            ).execute()
+
+            print("Arquivo atualizado com sucesso.")
+        else:
+            print("Criando arquivos dentro da pasta folder")
+            # Arquivo não existe, cria um novo
+            file_metadata = {
+                "name": form_name,
+                "parents": [parent_folder_id]
+            }
+            
+            media = MediaFileUpload(f'/tmp/output.csv')
+            request = service.files().create(
+                body=file_metadata,
+                media_body=media,
+                fields="id"
+            ).execute()
+            print("Deu certo, ", request)
 
     except HttpError as e:
         print("Error: ", str(e))
